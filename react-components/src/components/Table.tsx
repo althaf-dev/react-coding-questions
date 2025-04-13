3fc9649f925baa64ae6edda1066efe4743796741
Althaf <abdulla@innoventes.co>
2025-04-13 20:43:46 +0530

    Add sort option for the single field

--- react-components/src/components/Table.tsx
+++ react-components/src/components/Table.tsx
@@ -7,28 +7,71 @@ interface TableProps {
 }
 
 export function Table({ data }: TableProps) {
-
   const [debouncedSearch, setDebouncedSearch] = useState('');
- 
+  const [order, setOrder] = useState('asc');
+
   const columns = Array.from(
     new Set(data.flatMap((item) => Object.keys(item)))
   );
 
+  const handleSort = () => {
+    setOrder((prev) => (prev === 'asc' ? 'dsc' : 'asc'));
+  };
 
-  const searchedData = data.filter((item) => item.name.includes(debouncedSearch));
+
+  const searchedData = data.filter((item) =>
+    item.name.includes(debouncedSearch)
+  );
+
+  const sortedData = searchedData.sort((a,b)=>{
+    if(a.name>b.name && order === "asc"){
+        return 1;
+    }
+
+    if(a.name<b.name && order === "asc"){
+        return -1;
+    }
+  
+    if(a.name>b.name && order === "dsc"){
+        return -1;
+    }
+
+    if(a.name<b.name && order === "dsc"){
+        return 1;
+    }
+
+    return 1;
+  })
+
   return (
     <div className="table-container">
-     <Search setDebouncedSearch={setDebouncedSearch}/>
+      <Search setDebouncedSearch={setDebouncedSearch} />
       <table>
         <thead>
           <tr>
             {columns.map((col) => (
-              <th key={col}>{col}</th>
+              <th key={col}>
+                <span>{col}</span>
+                {col === 'name' && (
+                  <button onClick={handleSort}>
+                    <span
+                      style={{ color: order === 'asc' ? 'purple' : '#ddd' }}
+                    >
+                      {'\u2191'}
+                    </span>
+                    <span
+                      style={{ color: order === 'dsc' ? 'purple' : '#ddd' }}
+                    >
+                      {'\u2193'}
+                    </span>
+                  </button>
+                )}
+              </th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {searchedData.map((row) => (
+          {sortedData.map((row) => (
             <tr key={row.id}>
               {columns.map((col) => (
                 <td key={col}>
